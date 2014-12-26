3fe3c868b93d3a2c770ab54515c0b8c3e9960ac2
Tal Shorer <tal.shorer@gmail.com>
2014-12-26 14:23:01 +0200

    remove network from the run script (not working at the moment)
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,18 +2,18 @@
 
 cd $1 /dev/null
 
-NETIFACE=qemu1
+NETIFACE=qemu0
 
-tunctl -t $NETIFACE
-ifup $NETIFACE
+#tunctl -t $NETIFACE
+#ifup $NETIFACE
 qemu-system-i386 \
 	-hda rootfs.ext2 \
 	-kernel bzImage \
 	-m 512M \
 	-append "root=/dev/sda console=ttyS0" \
-	-nographic \
 	-localtime \
-	-net nic,vlan=0 \
-	-net tap,vlan=0,ifname=$NETIFACE
-ifdown $NETIFACE
-tunctl -d $NETIFACE
+	-serial stdio \
+#	-net nic,vlan=0 \
+#	-net tap,vlan=0,ifname=$NETIFACE
+#ifdown $NETIFACE
+#tunctl -d $NETIFACE

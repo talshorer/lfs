a3d8fe84a80d3bf6d8870f3cade61c28b4e45d41
Tal Shorer <tal.shorer@gmail.com>
2014-12-27 02:07:09 +0200

    automatic ssh is now working. the host keys are random keys generated by the vm and saved. the client's public key is copied from the compiling user's ~/.ssh/id_rsa.pub
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- scripts/preimage/copy_root.sh
+++ scripts/preimage/copy_root.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 TARGET=$1
-SOURCE=$(dirname $0)/../rootfs
+SOURCE=$(dirname $0)/../../rootfs
 find $SOURCE -name "*~" -exec rm -fv {} \;
 rsync -avz $SOURCE/ $TARGET
---
+++ scripts/preimage/ssh_key.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+TARGET=$1
+TARGET_DIR="$TARGET/root/.ssh"
+TARGET_FILE="$TARGET_DIR/authorized_keys"
+SSH_KEY_FILE="/home/$USER/.ssh/id_rsa.pub"
+if [[ ! -e "$SSH_KEY_FILE" ]]; then
+	echo "$SSH_KEY_FILE not found"
+	exit 0
+fi
+mkdir -p $TARGET_DIR
+chmod 700 $TARGET_DIR -v
+cp -fv $SSH_KEY_FILE $TARGET_FILE

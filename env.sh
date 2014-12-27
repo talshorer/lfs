4ff0563e483355975e6b307be7046feb7b16da87
Tal Shorer <tal.shorer@gmail.com>
2014-12-27 22:14:56 +0200

    mount debugfs during startup and added a script to configure env variables for compiling modules
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ env.sh
@@ -0,0 +1,8 @@
+export PATH="/home/tal/Development/lfs/buildroot/output/host/bin:/home/tal/Development/lfs/buildroot/output/host/sbin:/home/tal/Development/lfs/buildroot/output/host/usr/bin:/home/tal/Development/lfs/buildroot/output/host/usr/sbin:$PATH"
+export HOSTCC="/usr/bin/gcc"
+export HOSTCFLAGS=""
+export ARCH=i386
+export INSTALL_MOD_PATH=/home/tal/Development/lfs/buildroot/output/target
+export CROSS_COMPILE="/home/tal/Development/lfs/buildroot/output/host/usr/bin/i686-buildroot-linux-uclibc-"
+export DEPMOD=/home/tal/Development/lfs/buildroot/output/host/sbin/depmod
+export KERNEL=/home/tal/Development/lfs/buildroot/output/build/linux-custom

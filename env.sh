d091e09028ade366f8ea88e5319e53371406c4c3
Tal Shorer <tal.shorer@gmail.com>
2014-12-28 22:04:31 +0200

    properly configure the environment variables for module compilations on different machines
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
-export PATH="/home/tal/Development/lfs/buildroot/output/host/bin:/home/tal/Development/lfs/buildroot/output/host/sbin:/home/tal/Development/lfs/buildroot/output/host/usr/bin:/home/tal/Development/lfs/buildroot/output/host/usr/sbin:$PATH"
+LFS=$(realpath $(dirname $_))
+export PATH="$LFS/buildroot/output/host/bin:$LFS/buildroot/output/host/sbin:$LFS/buildroot/output/host/usr/bin:$LFS/buildroot/output/host/usr/sbin:$PATH"
 export HOSTCC="/usr/bin/gcc"
 export HOSTCFLAGS=""
 export ARCH=i386
-export INSTALL_MOD_PATH=/home/tal/Development/lfs/buildroot/output/target
-export CROSS_COMPILE="/home/tal/Development/lfs/buildroot/output/host/usr/bin/i686-buildroot-linux-uclibc-"
-export DEPMOD=/home/tal/Development/lfs/buildroot/output/host/sbin/depmod
-export KERNEL=/home/tal/Development/lfs/buildroot/output/build/linux-custom
+export INSTALL_MOD_PATH=$LFS/buildroot/output/target
+export CROSS_COMPILE="$LFS/buildroot/output/host/usr/bin/i686-buildroot-linux-uclibc-"
+export DEPMOD=$LFS/buildroot/output/host/sbin/depmod
+export KERNEL=$LFS/buildroot/output/build/linux-custom

d1a1ddb3f1a25f038e164aef88c40ca573e76c8c
Tal Shorer <tal.shorer@gmail.com>
2015-03-23 23:44:49 +0200

    mlc: minimize linux config - initial script
    Added the "main" script run_pylkc, which will be used with
    custom rules (additional scripts) to edit the linux configuration
    file automatically.
    
    A rule script that does nothing, nop, is also added with this patch.
    With this rule, run_pylkc will output the same configuration it's given.
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ minimize_linux_config/rule0_nop/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+
+def run(pylkc):
+    pass
---
+++ minimize_linux_config/run_pylkc.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import sys
+import os
+import collections
+import imp
+
+
+def main(pylkc, rule_func, output):
+    lfs = os.environ["LFS"]
+    conffile = os.path.join(lfs, "linux-config")
+    if output is None:
+        output = conffile
+    linux = os.path.join(lfs, "linux")
+    pylkc.init(linux)
+    pylkc.conf_parse(linux)
+    pylkc.conf_read(conffile)
+    rule_func(pylkc)
+    pylkc.conf_write(output)
+
+
+def _main():
+    import argparse
+    p = argparse.ArgumentParser()
+    rule_type = collections.namedtuple("Rule", ["file", "func"])
+    p.add_argument("--rule", type=(lambda s: rule_type(*s.split(":"))))
+    p.add_argument("--pylkc")
+    p.add_argument("-o", dest="output")
+    args = p.parse_args()
+    # allow path to start with "~/"
+    sys.path.append(os.path.expanduser(args.pylkc))
+    import pylkc
+    rule_func = getattr(__import__(args.rule.file), args.rule.func)
+    main(pylkc, rule_func, args.output)
+
+if __name__ == "__main__":
+    _main()

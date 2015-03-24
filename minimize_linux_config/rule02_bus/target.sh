573f05cc1cd9cb982415f510a3392842d785d913
Tal Shorer <tal.shorer@gmail.com>
2015-03-24 20:17:30 +0200

    mlc: rule02: target.sh: fix "ls" to be "ls -1", since the wc command counts lines
    This does not change the output
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- minimize_linux_config/rule02_bus/target.sh
+++ minimize_linux_config/rule02_bus/target.sh
@@ -4,7 +4,7 @@ SYSBUS=/sys/bus
 
 for bus in $(ls $SYSBUS); do
 	bus_dir=$SYSBUS/$bus
-	ndevices=$(ls $bus_dir/devices | wc -l)
+	ndevices=$(ls -1 $bus_dir/devices | wc -l)
 	[[ "$ndevices" == "0" ]] && echo $bus
 done
 

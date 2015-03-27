927966b4828690437530ef5452f0e935ad32285e
Tal Shorer <tal.shorer@gmail.com>
2015-03-27 14:08:41 +0300

    env: fix MOD_INSTALL_PATH
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

--- env.sh
+++ env.sh
@@ -1,11 +1,11 @@
 LFS=$(realpath $(dirname ${BASH_SOURCE[0]}))
 export HOST_ROOT=$LFS/buildroot/output/host
 export TARGET_ROOT=$LFS/buildroot/output/target
-export PATH="$HOST_ROOT/bin:$HOST_ROOT/sbin:$HOST_ROOT/usr/bin:$HOST_ROOT/usr/sbin:$PATH"
+export PATH="$PATH:$HOST_ROOT/bin:$HOST_ROOT/sbin:$HOST_ROOT/usr/bin:$HOST_ROOT/usr/sbin"
 export HOSTCC="/usr/bin/gcc"
 export HOSTCFLAGS=""
 export ARCH=i386
-export INSTALL_MOD_PATH=$INSTALL_MOD_PATH
+export INSTALL_MOD_PATH=$TARGET_ROOT
 export CROSS_COMPILE="$HOST_ROOT/usr/bin/i686-buildroot-linux-uclibc-"
 export DEPMOD=$HOST_ROOT/sbin/depmod
 export KERNEL=$LFS/buildroot/output/build/linux-custom
